0c2e24c0be3e4afebc4bfeb9b5c893cfd16d9ede
rahatmaini <rahatmaini@live.com>
2015-10-18 01:57:44 -0400

    j

--- firmware/Adafruit_PN532.cpp
+++ firmware/Adafruit_PN532.cpp
@@ -49,7 +49,7 @@ byte pn532ack[] = {0x00, 0x00, 0xFF, 0x00, 0xFF, 0x00};
 byte pn532response_firmwarevers[] = {0x00, 0xFF, 0x06, 0xFA, 0xD5, 0x03};
 
 // Uncomment these lines to enable debug output for PN532(SPI) and/or MIFARE related code
- #define PN532DEBUG
+ //#define PN532DEBUG
 // #define MIFAREDEBUG
 
 #define PN532_PACKBUFFSIZ 64
